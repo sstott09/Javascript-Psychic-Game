b4e0daad8673685ff70969606052c1d2c88646d7
Steven Stot <sstott09@yahoo.com>
2019-10-19 21:09:34 -0600

    Final Version

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,7 +1,6 @@
 //Array of possible computer choices
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
 //Variables for tracking players wins, losses, & guesses left
 var wins = 0;
 var losses = 0;
@@ -10,42 +9,42 @@ var lettersGuessed = [];
 var computerGuess = [];
 
 //Game
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 	var playerGuess = event.key;
 	lettersGuessed.push(playerGuess);
 	console.log(computerGuess[0]);
 
-if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
-	wins++;
-	guessesLeft = 9;
-	lettersGuessed.length = 0;
-    computerGuess.length = 0;
-    
-	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	computerGuess.push(compGuess);
-	console.log(computerGuess[0]);
-}
+	if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
+		wins++;
+		guessesLeft = 9;
+		lettersGuessed.length = 0;
+		computerGuess.length = 0;
 
-else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
-	guessesLeft = guessesLeft-1;
-}
+		var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+		computerGuess.push(compGuess);
+		console.log(computerGuess[0]);
+	}
 
-else {
-	losses++;
-	guessesLeft = 9;
-	lettersGuessed.length = 0;
-	computerGuess.length = 0;
-	var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	computerGuess.push(compGuess);
-	console.log(computerGuess[0]);
-}
+	else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
+		guessesLeft = guessesLeft - 1;
+	}
 
-var html = "<h3>Guess what letter I'm thinking of!</h3>" +
-          "<h3>Wins: " + wins + "</h3>" +
-          "<h3>Losses: " + losses + "</h3>" +
-          "<h3>Guesses left: " + guessesLeft + "</h3>" +
-          "<h3>Your guesses so far: " + lettersGuessed + "</h3>";
- 
-document.querySelector("#game").innerHTML = html;
+	else {
+		losses++;
+		guessesLeft = 9;
+		lettersGuessed.length = 0;
+		computerGuess.length = 0;
+		var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+		computerGuess.push(compGuess);
+		console.log(computerGuess[0]);
+	}
+
+	var html = "<h2>Guess what letter I'm thinking of!</h2>" +
+		"<h3>Wins: " + wins + "</h3>" +
+		"<h3>Losses: " + losses + "</h3>" +
+		"<h3>Guesses left: " + guessesLeft + "</h3>" +
+		"<h3>Your guesses so far: " + lettersGuessed + "</h3>";
+
+	document.querySelector("#game").innerHTML = html;
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# 03-Javascript
-Psychic Game
+## University of Denver 
+## Class: DU_VIRT_FSF_PT_09_2019_U.O.MW
+## Program: Full Stack Flex Online Part-Time
+## 03-Javascript Assignment One (Psychic Game) - Basic
+...........................................................................................................
+
+### Overview
+In this assignment, I've created a simple yet compelling computer game. 
+
+1. This app runs in the browser, and features dynamically updated HTML and CSS powered by JavaScript code. 
+2. Essentially, the app randomly picks a letter, and the user has to guess which letter the app chose.
+
+### Key Topics
+Code comments
+console
+Variables
+Types
+Booleans
+Numbers
+Strings
+Arrays
+Conditional statements
+Equality
+Logical, comparison and ternary operators
+Functions
+Alerts, confirms, prompts
+Global vs local scope
+
+© 2019, Steven M Stott
+All rights reserved.
